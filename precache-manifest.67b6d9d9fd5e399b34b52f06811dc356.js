86a7af9abf1708a68646bbb0a992cfa9fc2548aa
jongseok-98 <whdtlr202@gmail.com>
2021-05-23 02:25:20 +0900

    Updates

---
+++ precache-manifest.67b6d9d9fd5e399b34b52f06811dc356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c326fd3d2ac50b418c8e6f94a8a8c1",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "80bae5e65cec7f49ab3c",
+    "url": "/twitter-clone/static/css/main.8f0a2bd3.chunk.css"
+  },
+  {
+    "revision": "9b184cc40f7642eed359",
+    "url": "/twitter-clone/static/js/2.7d25446a.chunk.js"
+  },
+  {
+    "revision": "78574d4557bd67e5be88f3f92b314894",
+    "url": "/twitter-clone/static/js/2.7d25446a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bae5e65cec7f49ab3c",
+    "url": "/twitter-clone/static/js/main.74dbe162.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
